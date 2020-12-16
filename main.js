f37daa9e6fdd3147939efcb0b34c6de43c55f2bc
anookday <anookday@gmail.com>
2020-12-16 17:29:00 +0900

    initial reacct app

---
+++ main.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+const path = require('path');
+const isDev = require('electron-is-dev');
+require('electron-reload');
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    fullscreen: true,
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const renderContent = () => {
+    let username = 'elonmusk';
+    let message = 'EZ Clap EZ Clap EZ Clap EZ Clap EZ Clap EZ Clap';
+    return (
+      <p className="chatMessage">
+        <b className="text-green">{username}: </b>
+        {message}
+      </p>
+    );
+  };
+
+  return <div className="appContainer">{renderContent()}</div>;
+};
 
 export default App;
